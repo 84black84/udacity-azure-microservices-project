45a7b134f0148f9cd75209fc7c1c777cd95c8d50
Christos Mavrogiannis <Christos.Mavrogiannis@ottobockde.onmicrosoft.com>
2022-05-29 22:17:58 +0200

    Part 4 - Fixing issues with logging on event grid handler

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -2,13 +2,7 @@ import json
 import logging
 import azure.functions as func
 
-
 def main(event: func.EventGridEvent):
-
-	logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
 
     result = json.dumps({
         'id': event.id,
@@ -20,6 +14,3 @@ def main(event: func.EventGridEvent):
 
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
-
-
-
